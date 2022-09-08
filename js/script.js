7aa8a2d12b6159d3c14b48b34da7b69e5e421c50
duongKDR <106894490+duongKDR@users.noreply.github.com>
2022-09-08 22:32:47 +0700

    last update

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ var flipCounter,
   timer,
   gameOn = false;
 var counterClick = 0;
-let clickIndex=-1;
+let clickIndex = -1;
 let totalOpeningCard = [];
 let checkDuplicate = [];
-let CONDITIONWINING = 4;
+let CONDITIONWINING = 18;
 let winning = false;
 
 function resetGame() {
-  igNore=[];
+  igNore = [];
   var elements = document.getElementsByClassName("card");
   while (elements.length > 0) {
     elements[0].parentNode.removeChild(elements[0]);
@@ -21,7 +21,7 @@ function resetGame() {
 }
 const b = () => {
   totalOpeningCard = [];
-  const loss = document.getElementById("loss")
+  const loss = document.getElementById("loss");
   const win = document.getElementById("win");
   if (loss) {
     loss.classList.remove("visible");
@@ -34,10 +34,7 @@ const b = () => {
   init();
 };
 
-
 var skillCard = [];
-
-
 
 function showCountdown() {
   return setInterval(() => {
@@ -67,7 +64,6 @@ function showWin() {
   clearInterval(countdown);
 }
 function init() {
-
   gameOn = true;
   count = 0;
   // flipCounter = 0;
@@ -173,17 +169,14 @@ function init() {
   console.log(skillCard);
   startTimer(minutes, display);
   showTags();
-
 }
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-
 function showTags() {
   console.log("show.....");
   let Str = "";
-
 
   for (let i = 0; i < skillCard.length; i++) {
     const element = skillCard[i];
@@ -211,55 +204,48 @@ function showTags() {
   document.getElementById("tags").innerHTML = Str;
 }
 
-
 function toogleMask(id) {
-  
-  if(igNore.includes(skillCard[id].id)) return  ;
+  if (igNore.includes(skillCard[id].id)) return;
   memoryCard.push(skillCard[id].id);
-  
-  
-  totalOpeningCard.push(skillCard[id].id);
-  // checkDuplicate.push(id);
-  
   counterClick = counterClick + 1;
+  totalOpeningCard.push(skillCard[id].id);
   document.getElementById(id).classList.toggle("hidden");
-  // console.log(checkDuplicate);
-  // console.log(totalOpenmingCard)
-  // console.log(igNore);s
-  // console.log(totalOpenmingCard.length);
+  console.log(totalOpeningCard);
 
 }
-
-
 
 function update(delta) {
   if (counterClick === 2) {
     counterClick = 0;
     if (memoryCard[0] === memoryCard[1] && memoryCard.length === 2) {
       igNore.push(memoryCard[0]);
-      console.log(igNore);
+      // console.log(igNore);
       console.log(memoryCard);
       if (totalOpeningCard.length === CONDITIONWINING) {
         winning = true;
         showWin();
         totalOpeningCard = [];
       }
-      // if(clickIndex.push(memoryCard)!=clickIndex.push(memoryCard))return toogleMask;
-    }
-    else {
-      setTimeout(() => {
-        const tags = document.querySelectorAll(".mask");
-        totalOpeningCard = [];
-        igNore = [];
-        for (let index = 0; index < tags.length; index++) {
-          const element = tags[index];
-          if (!element.classList.contains("hidden")) {
-            element.classList.add("hidden");
-          }
+    } else {
+      // loi ko trừ 1 can trừ 1 totalopen
+      // totalOpeningCard = totalOpeningCard-1;
+      const tags = document.querySelectorAll(".mask");
+
+      igNore = [];
+      for (let index = 0; index < tags.length; index++) {
+        const element = tags[index];
+        if (
+          !element.classList.contains("hidden") &&
+          memoryCard.includes(skillCard[Number(element.id)].id)
+        ) {
+          element.classList.add("hidden");
+          totalOpeningCard = totalOpeningCard.filter(
+            (i) => i !== skillCard[Number(element.id)].id
+          );
         }
-      }, 500);
+      }
     }
-   
+
     memoryCard = []; // clear memoryCard
   }
 }
@@ -269,7 +255,6 @@ function confirm() {
   if (winning) {
     winning = false;
   }
-
 }
 
 let lastUpdate = Date.now();
